b2c64ddb2e598b96c3732a5f2b2fa796b2b1dd9c
wuodar <kac.wlodarczyk@gmail.com>
2021-04-17 20:39:29 +0200

    added base skeleton

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import streamlit as st
 
+st.sidebar.title("Navigation")
+
+treshold = st.sidebar.slider("Select treshold", 0, 300)
+
 """
-# GPT based app
+# Digital garden
+
+### _Queries_: \n
+_How is the first paper similar to the second paper?_\n
+_How is the first paper different from the second paper?_\n
+_What is the main of the first paper?_\n
+_What is the main topic of the second paper?_\n
+_Why does the first paper belong to class X?_\n
+_Why does the second paper belong to class X?_\n
+
 """
 
 text = st.text_input("Enter some text here.")
+
 
 f"{text}" if text else ""
 
