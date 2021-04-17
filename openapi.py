ea314063c62fe457762a3bdcf2e347952ebfa838
Adrian Zielinski <adrianziel24@wp.pl>
2021-04-17 14:36:13 +0200

    Keypoints

--- openapi.py
+++ openapi.py
@@ -12,10 +12,25 @@ def get_keywords(text: str):
     response = openai.Completion.create(
       engine=engine,
       prompt=f'{text} \n Keywords:',
-      temperature=0.3,
+      temperature=0.1,
       max_tokens=60,
       top_p=1.0,
       frequency_penalty=0.8,
+      presence_penalty=0.0,
+      stop=["\n"]
+    )
+
+    return response
+
+
+def get_keypoints(text: str):
+    response = openai.Completion.create(
+      engine=engine,
+      prompt=f'What are the key points from this text? \n {text}:',
+      temperature=0.5,
+      max_tokens=100,
+      top_p=1.0,
+      frequency_penalty=0.0,
       presence_penalty=0.0,
       stop=["\n"]
     )
