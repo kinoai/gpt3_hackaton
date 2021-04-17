c7f76885de5a5a3401bbc10892183d52ddb5eba0
wuodar <kac.wlodarczyk@gmail.com>
2021-04-17 23:51:09 +0200

    fix literal

--- upload_files.py
+++ upload_files.py
@@ -16,7 +16,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 with open("./data/papers_full.pickle", "rb") as handle:
     data = pickle.load(handle)
 
-docs = "\r\n".join(
+docs = "\n".join(
     [
         doc["title"].replace("\n", " ")
         + " "
@@ -32,23 +32,18 @@ print(request)
 file_path = "./data/file.txt"
 
 with jsonlines.open(file_path, mode="w") as writer:
-    writer.write(docs)
-    # writer.write({"text": docs})
+    # writer.write(json.dumps({"text": docs}))
+    writer.write({"text": docs})
 
 
 # openai.File.create(file=open(file_path), purpose="search")
 # %%
+# doesn't work : <
 openai.Engine("davinci").search(
-    file="file-Fx5538zS0UhhXuEdpUHUMVUF",
+    file="fileID",
     query="Which article is from the Reinforcement Learning field?",
 )
 
 # %%
 openai.File.list()
-# openai.File("file-y4z8nlkYceIAv9e4oWL1PKDe").delete()
-# %%
-with open(file_path) as f:
-    d = json.load(f)
-
-print(d)
-# %%
+# openai.File("fileID").delete()
