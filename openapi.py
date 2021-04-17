d0c31159003c523ed2469ed52b555d9e28b48c34
wuodar <kac.wlodarczyk@gmail.com>
2021-04-17 16:37:17 +0200

    added semantic search example

--- openapi.py
+++ openapi.py
@@ -1,23 +1,31 @@
+# %%
+import json
 import os
+import pickle
 
 import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-engine = 'ada'
 
+# %%
+with open("./data/papers.pickle", "rb") as handle:
+    data = pickle.load(handle)
+# %%
+# with open("file.jsonl", "w") as outfile:
+#     json.dump(text, outfile)
 
-def get_keywords(text: str):
-    response = openai.Completion.create(
-      engine=engine,
-      prompt=f'{text} \n Keywords:',
-      temperature=0.3,
-      max_tokens=60,
-      top_p=1.0,
-      frequency_penalty=0.8,
-      presence_penalty=0.0,
-      stop=["\n"]
-    )
+# %%
+# openai.File.create(file=open("file.jsonl"), purpose="answers")
+# %%
+docs = [
+    art["title"].replace("\n", " ") + " " + art["abstract"].replace("\n", " ")
+    for art in data
+]
 
-    return response
+response = openai.Engine("davinci").search(
+    documents=docs, query="Parameter optimization"
+)
+# %%
+print(response)
